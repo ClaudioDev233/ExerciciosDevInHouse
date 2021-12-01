d6e822051831b3ea8d280e44526d6c83bb9bfd7a
Claudio <you@example.com>
2021-11-30 21:58:58 -0300

    Exercicios feitos em aula 30/11

---
+++ Semana7/ExerciciosRestSpread/todosExercicios.js
@@ -0,0 +1,36 @@
+const frameworks = ['vuejs', "angularjs", "reactjs"]
+
+const [,,esseframe] = frameworks;
+
+const personagem ={
+    nome: 'Sam',
+    nascimento: '11xxx',
+    familia: {
+        esposa: "Rosinha",
+        melhorAmigo: 'Frodo'
+    }
+};
+
+const historiaDoPersonagem= ({nome, familia:{esposa,melhorAmigo}})=>{ 
+    return `Meu nome é ${nome} e sou casado com ${esposa}. Gosto de dar um role com meu amigo ${melhorAmigo}`
+ }
+
+const frutas = ["morango", "pessego", "banana"]
+const legumes = ["cenoura", "batata","abobrinha"]
+
+const alimentos = [...frutas , ...legumes]
+
+const usuario ={
+    nome: "Joana",
+    sobrenome: "Carvalho",
+    interesses: ["leitura", 'jardinagem', 'internet das coisas'],
+    familia: {
+        pai:'Claudio',
+        mae: 'Odete',
+        irma:'Lorena'
+    }
+}
+
+const falaMoca = ({nome, sobrenome, interesses: [interesse], familia: {mae}}) =>{ 
+    return `Olá, meu nome é ${nome} ${sobrenome} e meu principal hobbie é ${interesse}. O nome da minha mãe é ${mae}`
+}
