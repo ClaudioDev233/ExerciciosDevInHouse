9aa82d74493afcfc79900c1b53cade9e9af40286
Claudio <you@example.com>
2021-12-01 15:07:41 -0300

    ex10 brabo

---
+++ Semana5/exercicio10/modules/classes.js
@@ -0,0 +1,10 @@
+class Produto {
+    constructor(nome,imagem, valor,parcelaMax){
+        this.nome = nome,
+        this.imagem = imagem;
+        this.valor = valor;
+        this.parcelaMax = parcelaMax;
+        
+    }
+}
+ export default Produto
---
+++ Semana6/ex3.js
@@ -0,0 +1,16 @@
+const nomeCompleto = ((nome,sobrenome) =>{ 
+     if(nome == undefined || nome ==''){
+        nome = "Jane";
+        
+    } 
+    if(sobrenome == undefined){
+        sobrenome ="Doe"
+    }
+    if (nome == undefined && sobrenome == undefined){
+        nome = 'Jane';
+        sobrenome='Doe';
+    }
+    
+    return `Olá meu nome é:${nome} ${sobrenome}`
+});
+console.log(nomeCompleto('','Junior'))
